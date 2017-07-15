c1d8aceff472ccc179023aba2f2e6e063b3eedd3
Veiko-PC\Veiko <veiko.teekel@gmail.com>
2017-07-15 22:13:17 +0300

    owli eemaldamine teemast

--- js/scripts.js
+++ js/scripts.js
@@ -41,29 +41,7 @@ $('#navmenu li:first-child a').addClass('selected');
 	$(this).addClass('selected');
 });*/
 
-$(".owl-carousel").owlCarousel({
 
-	/*items: 1,*/
-	/*autoplay: true,
-	loop: true,
-	singleItem:true,
-	autoplayTimeout: 10000,*/
-	/*slideSpeed : 300,*/
-	items:1,
-	paginationSpeed : 400,
-	autoPlay : 3000,
-	stopOnHover : true,
-	navigation: true,
-/*	paginationSpeed : 1000,
-	goToFirstSpeed : 2000,*/
-	autoHeight : true,
-	autoPlay : true,
-	singleItem:true,
-	animateOut: 'fadeOut',
-	animateIn: 'fadeIn'
-
-});
-$(".owl-item:first-child .item").addClass('animate_center');
 
 
 
