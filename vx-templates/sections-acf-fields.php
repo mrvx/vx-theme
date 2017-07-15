7ff9809837c2daacc1ea53ce488b79635aab92e9
Veiko-PC\Veiko <veiko.teekel@gmail.com>
2017-07-15 20:32:37 +0300

    lisasin sketioonid süsteemile php-ga väljad

--- vx-templates/sections-acf-fields.php
+++ vx-templates/sections-acf-fields.php
@@ -3,6 +3,7 @@ if( function_exists('acf_add_local_field_group') ):
 
 acf_add_local_field_group(array (
 	'key' => 'group_58581ba03ddd6',
+	'title' => 'Sektsioonid',
 	'fields' => array (
 		array (
 			'key' => 'field_58581c147c624',
